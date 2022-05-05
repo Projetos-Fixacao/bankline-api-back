e860cc70a5091321ce11b9dd4308c66cea0cf6ad
alanadiastech <pict.asd@gmail.com>
2022-05-05 17:59:52 -0300

    fim do back etapa 1

---
+++ src/main/java/com/dio/santander/bankline/api/controller/CorrentistaController.java
@@ -0,0 +1,36 @@
+package com.dio.santander.bankline.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dio.santander.bankline.api.dto.NovoCorrentista;
+import com.dio.santander.bankline.api.model.Correntista;
+import com.dio.santander.bankline.api.repository.CorrentistaRepository;
+import com.dio.santander.bankline.api.service.CorrentistaService;
+
+@RestController
+@RequestMapping("/correntistas")
+public class CorrentistaController {
+	
+	@Autowired
+	private CorrentistaRepository correntistaRepository;
+	
+	@Autowired
+	private CorrentistaService correntistaService;
+	
+	@GetMapping
+	public List<Correntista> findAll(){
+		return correntistaRepository.findAll();
+	}
+	
+	@PostMapping
+	public void save(@RequestBody NovoCorrentista novoCorrentista) {
+		correntistaService.save(novoCorrentista);
+	}
+}
